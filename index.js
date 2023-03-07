350f00853361f2774e4136a046b1b6ae2b6746ee
ZAdrianZ <104173720+ZAdrianZ@users.noreply.github.com>
2023-03-07 07:33:01 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let botonA1 = "Hola mi nombre es";
+let botonA2 = "Adrian";
+let botonB1 = "Hola mi @gmail es";
+let botonB2 = "777@gmail.com";
+let botonC1 = "Hola mi cumpleaños es";
+let botonC2 = "06/06/2006";
+let botonD1 = "Hola vivo en";
+let botonD2 = "Buenos aires";
+let botonE1 = "Hola mi tell es";
+let botonE2 = "+77 335566";
+let botonF1 = "Hola mi password es";
+let botonF2 = "555-555";
+/*VARIABLES PARA ONCLICKS DE BOTONES*/ 
